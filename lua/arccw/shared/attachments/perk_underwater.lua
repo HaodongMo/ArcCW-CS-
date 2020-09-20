3599211a468bd3dbdf698b209fffbf75b4c62434
Fesiug <publicfesiug@outlook.com>
2020-09-20 15:17:17 -0400

    j

--- lua/arccw/shared/attachments/perk_underwater.lua
+++ lua/arccw/shared/attachments/perk_underwater.lua
@@ -2,10 +2,10 @@ att.PrintName = "Diver"
 att.Icon = Material("entities/acwatt_perk_underwater.png")
 att.Description = "Allows gun to shoot underwater."
 att.Desc_Pros = {
-    "pro.perk_underwater"
 }
 att.Desc_Cons = {
 }
+att.AutoStats = true
 att.Slot = "perk"
 
 att.Override_CanFireUnderwater = true
