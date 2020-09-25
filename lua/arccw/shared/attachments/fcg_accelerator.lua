c4e93842a1409522f5f27eaa89afca8c43d7c937
Fesiug <publicfesiug@outlook.com>
2020-09-25 14:42:36 -0400

    Update fcg_accelerator.lua

--- lua/arccw/shared/attachments/fcg_accelerator.lua
+++ lua/arccw/shared/attachments/fcg_accelerator.lua
@@ -25,7 +25,7 @@ att.Override_Firemodes = {
 att.Hook_ModifyRPM = function(wep, delay)
     local max = math.min(7, wep:GetCapacity())
 
-    local delta = wep.BurstCount / max
+    local delta = wep:GetBurstCount() / max
 
     local mult = Lerp(delta, 0.5, 1.25)
 
